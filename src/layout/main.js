074423a0e3f1beb3e63a0e8848d8b25b9125594f
naumanQP <90209160+naumanQP@users.noreply.github.com>
2022-03-18 16:46:09 +0500

    first commit

---
+++ src/layout/main.js
@@ -0,0 +1,26 @@
+
+import { useEffect, useState } from "react";
+import ResponsiveAppBar from "../components/navbar";
+import ClippedDrawer from "../components/sidenav";
+
+
+export function Main() {
+
+
+
+    return (
+        <>
+            <div className='bg-grey'>
+               
+                <ClippedDrawer />
+                {/* Nauman */}
+
+            </div>
+         
+
+
+        </>
+
+    )
+}
+export default Main
